c2ae251c1dbee420b53bc3ac1b245cbb4f546fdc
TimKorolev <36112906+TimKorolev@users.noreply.github.com>
2019-12-20 12:28:22 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+let btnGetForPrint = document.querySelector('.getForPrint');
+btnGetForPrint.addEventListener('click', function(){allert('CLICK!')});
+btnGetForPrint.addEventListener('click', function{document.querySelector('.getForPrint').style.backgroundColor = 'red'});
