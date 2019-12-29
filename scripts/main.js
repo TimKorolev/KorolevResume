09803b35bcadfb4e9ddd96254294d8e6d3039c9f
Timofei <you@example.com>
2019-12-30 01:02:37 +0300

    Форматирование

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
-let btnGetForPrint = document.querySelector('.getForPrint');
-btnGetForPrint.addEventListener('click', function(){allert('CLICK!')});
-btnGetForPrint.addEventListener('click', function{document.querySelector('.getForPrint').style.backgroundColor = 'red'});
+let btnFeedback = document.querySelector('.feedback');
+let feedbackPopup = document.querySelector('.feedbackPopup');
+let closeFeedbackPopupBtn = document.querySelector('.feedbackPopup .close');
+
+btnFeedback.addEventListener('click', function(){feedbackPopup.style.display = 'block'});
+closeFeedbackPopupBtn.addEventListener('click', function(){feedbackPopup.style.display = 'none'});
+
+window.onclick = function (e) {
+    if(e.target == feedbackPopup){
+        feedbackPopup.style.display="none";
+    }
+}
