52edd79c06d7cadc3e6993aa52e4ec694eabe341
Timofei <tkkorolev@yanex.ru>
2019-12-31 22:26:42 +0300

    json

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,19 @@
 $.getJSON('data/data.json', function (data) {
     let name = data.name;
     let profession = data.profession;
-    // let phone = data.contacts.phone;
-    // let email = data.contacts.email;
+    let phone = data.contacts.phone;
+    let email = data.contacts.email;
     let skills = data.skills;
     let educations = data.educations;
+    let experiences = data.experience;
+    let aboutMe = data.aboutMe;
 
     document.getElementById("name").innerHTML = name;
     document.getElementById("profession").innerHTML = profession;
     // document.getElementById("phone").innerHTML = phone;
     // document.getElementById("email").innerHTML = email;
 
-    var skillData ='';
+    var skillData = '';
     for (let i = 0; i < skills.length; i++) {
         skillData += '<div class=\"BorderNameplate\">\n' +
             '                <p>' + skills[i] + '</p>\n' +
@@ -19,7 +21,7 @@ $.getJSON('data/data.json', function (data) {
     }
     document.getElementById("skills").innerHTML = skillData;
 
-    var educationData='';
+    var educationData = '';
     for (let i = 0; i < educations.length; i++) {
         educationData += '<div class="BlackBorder">\n' +
             '<h1>' + educations[i].university + ', ' + educations[i].department + '</h1>\n' +
@@ -29,6 +31,26 @@ $.getJSON('data/data.json', function (data) {
             '</div>\n';
     }
     document.getElementById("education").innerHTML = educationData;
+
+    var experienceData = '';
+    for (let i = 0; i < experiences.length; i++) {
+        var exp = experiences[i].since + experience.to === "" ? '' : ' to ' + experiences[i].since;
+        experienceData +=
+            '<div class="BlackBorder">\n' +
+            '            <h1>' + experiences[i].employer + ', ' + experiences[i].position + '</h1>\n' +
+            '            <div class="Nameplate Worked">\n' +
+            '                <h1> Since ' + exp + '</h1>\n' +
+            '            </div>\n' +
+            '            <div class="description">\n' +
+            '                <ul>\n' +
+            experiences[i].description +
+            '                </ul>\n' +
+            '            </div>\n' +
+            '        </div>';
+    }
+    document.getElementById("experience").innerHTML = experienceData;
+
+    document.getElementById("aboutMe").innerHTML = aboutMe;
 });
 
 
